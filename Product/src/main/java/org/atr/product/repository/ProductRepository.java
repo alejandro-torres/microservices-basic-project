8c24430a1ae6d7861664c843e77dd2b93cbb31cb
alejandro-torres <33754925+alejandro-torres@users.noreply.github.com>
2023-06-20 23:50:22 +0200

    added repository test and Query :D

--- Product/src/main/java/org/atr/product/repository/ProductRepository.java
+++ Product/src/main/java/org/atr/product/repository/ProductRepository.java
@@ -2,11 +2,16 @@ package org.atr.product.repository;
 
 import org.atr.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-
+    @Query("SELECT p FROM Product p WHERE p.name = (:name) ORDER BY p.value DESC")
+    List<Product> getProductByNameOrderByValueDesc(@Param("name") String name);
 
 }
--- Product/src/test/java/org/atr/product/repository/ProductRepositoryTest.java
+++ Product/src/test/java/org/atr/product/repository/ProductRepositoryTest.java
@@ -1,11 +1,13 @@
 package org.atr.product.repository;
 
 import org.atr.product.entity.Product;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,6 +21,29 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository underTest;
 
+    static List<Product> unorderedProductList;
+    static List<Product> orderedAndFilteredProductList;
+
+
+
+    @BeforeAll
+    static void setUp(){
+        unorderedProductList = Arrays.asList(
+                Product.builder().name("SaltSplash").value(BigDecimal.valueOf(2)).build(),
+                Product.builder().name("SaltSplash").value(BigDecimal.valueOf(3)).build(),
+                Product.builder().name("SaltSplash").value(BigDecimal.valueOf(-2)).build(),
+                Product.builder().name("SaltSplash").value(BigDecimal.valueOf(7)).build(),
+                Product.builder().name("SaltSplash").value(BigDecimal.valueOf(500)).build(),
+                Product.builder().name("Salt").value(BigDecimal.valueOf(23)).build()
+        );
+        orderedAndFilteredProductList = Arrays.asList(
+                Product.builder().id(6).name("SaltSplash").value(BigDecimal.valueOf(500)).build(),
+                Product.builder().id(5).name("SaltSplash").value(BigDecimal.valueOf(7)).build(),
+                Product.builder().id(3).name("SaltSplash").value(BigDecimal.valueOf(3)).build(),
+                Product.builder().id(2).name("SaltSplash").value(BigDecimal.valueOf(2)).build(),
+                Product.builder().id(4).name("SaltSplash").value(BigDecimal.valueOf(-2)).build()
+        );
+    }
 
     @Test
     void itShouldSaveNewProduct() {
@@ -32,14 +57,31 @@ class ProductRepositoryTest {
                 .build();
 
         //When
-        underTest.save(product);
+        Product productSaved = underTest.save(product);
 
         //Then
         Optional<Product> optionalProduct = underTest.findById(product.getId());
         assertThat(optionalProduct)
                 .isPresent()
                 .hasValueSatisfying(p -> {
-                   assertThat(p).isEqualTo(product);
+                    assertThat(p).isEqualTo(product);
                 });
     }
+
+    @Test
+    void itShouldGetProductByNameOrderByValueDesc() {
+        // Given a product name
+        String name = "SaltSplash";
+
+        // ... a list of Products
+        underTest.saveAll(unorderedProductList);
+
+        //When
+        List<Product> productList = underTest.getProductByNameOrderByValueDesc(name);
+
+        //Then
+        assertThat(productList).isEqualTo(orderedAndFilteredProductList);
+    }
+
+
 }
